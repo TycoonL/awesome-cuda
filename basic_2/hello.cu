575879757e4d567a9c13c655f5c329041125d1d9
TycoonL <42563899+TycoonL@users.noreply.github.com>
2023-04-09 13:31:35 +0800

    Update hello.cu

--- basic_2/hello.cu
+++ basic_2/hello.cu
@@ -3,13 +3,12 @@
 #include "iostream"
 #include "cuda.h"
 
-//ºËº¯Êý
+//核函数
 __global__ void hello_from_device()
 {
 	int bid_x = blockIdx.x;
 	int bid_y = blockIdx.y;
 	int tid = threadIdx.x;
-	//int threadID = blockIdx.x*blockDim.x + threadIdx.x;
 	printf("hello world from block (%d,%d) and thread %d\n", bid_x,bid_y, tid);
 }
 
@@ -18,6 +17,6 @@ int main(void)
 {
 	dim3 gridsize(2, 2);
 	hello_from_device<<<gridsize,3 >>>();
-	cudaDeviceSynchronize();//Í¬²½Ö÷»úÓëÉè±¸
+	cudaDeviceSynchronize();//同步主机与设备
 	return 0;
 }
